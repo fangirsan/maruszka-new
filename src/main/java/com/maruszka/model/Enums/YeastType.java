027e85a9947a8797aba5cb958aae970ae9811b33
Fangir <fangirsan@gmail.com>
2019-07-11 19:09:06 +0200

    Adding description to YeastType Enum. Close #20.

--- src/main/java/com/maruszka/model/Enums/YeastType.java
+++ src/main/java/com/maruszka/model/Enums/YeastType.java
@@ -1,6 +1,16 @@
 package com.maruszka.model.Enums;
 
 public enum YeastType {
-	Dry, Liquid
-	
+	Dry("Dry yeast"),
+	Liquid("Liquid yeast");
+
+	private String description;
+
+	YeastType(final String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
 }
