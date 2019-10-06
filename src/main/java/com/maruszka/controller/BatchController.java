5af0622ae15c15fa66093dc9285921f5a59e3a80
Fangir <fangirsan@gmail.com>
2019-10-06 21:27:50 +0200

    #49: Initial comit for this branch

--- src/main/java/com/maruszka/controller/BatchController.java
+++ src/main/java/com/maruszka/controller/BatchController.java
@@ -5,7 +5,9 @@ import com.maruszka.services.BatchService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -19,14 +21,28 @@ class BatchController {
         this.batchService = batchService;
     }
 
+    @GetMapping("/{batchId}")
+    public ModelAndView showBatch(@PathVariable("batchId") Long batchId) {
+
+        ModelAndView mav = new ModelAndView("batch/batch-show");
+        Batch batchToShow = batchService.findById(batchId);
+
+//        if (batchToShow.getCountry() == null) {
+//            batchToShow.setCountry(countryService.findByCountryName("N/A"));
+//        }
+
+        mav.addObject(batchService.findById(batchId));
+        mav.addObject("malts", batchToShow.getMalts());
+
+        return mav;
+    }
+
     @GetMapping("/list")
     public String getBatches(Model theModel) {
 
         Set<Batch> theBatches = batchService.findByOrderByBatchNumberAsc();
-
         theModel.addAttribute("batches", theBatches);
-
-        return "batch-list";
+        return "batch/batch-list";
     }
 
 }
